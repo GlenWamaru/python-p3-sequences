a552bea30a9987672093bb0d9e1a3b7cc4fca7c3
SengokuGarp <garpirungu@gmail.com>
2024-03-03 11:21:04 +0300

    Initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
+def generate_fibonacci(length):
+    if length == 0:
+        return []
+    elif length == 1:
+        return [0]
+
+    fibonacci_sequence = [0, 1]
+
+    while len(fibonacci_sequence) < length:
+        next_fibonacci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_fibonacci)
+
+    return fibonacci_sequence
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = generate_fibonacci(length)
+    print(fibonacci_sequence)
+
+# Test the function
+print_fibonacci(9)
